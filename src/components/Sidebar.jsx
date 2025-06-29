702e3d37645139c194f776ee79aa771f0e30d79f
jenishdabhi <jenishdabhi707@gmail.com>
2025-06-29 23:55:28 +0530

    edit profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,19 +2,32 @@ import React from "react";
 import assets from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 const Sidebar = ({ selecteduser, setSelectedUser }) => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selecteduser ? 'max-md:hidden' : ''}`}>
+    <div
+      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+        selecteduser ? "max-md:hidden" : ""
+      }`}
+    >
       <div className="pb-5">
         <div className="flex justify-between item-center">
           <img className="max-w-40" src={assets.logo} />
           <div className="relative py-2 group">
-                <img src={assets.menu_icon} alt="Menu" className="max-h-5 cursor-pointer"/>
-                <div className="absolute top-full right-0 z-30 w-32 p-5 round-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
-                    <p className="cursor-pointer text-sm" onClick={() => navigate('/profile')}>Edit Profile</p>
-                    <hr className="my-2 border-t border-gray-500" />
-                    <p className="cursor-pointer text-sm">Logout</p>
-                </div>
+            <img
+              src={assets.menu_icon}
+              alt="Menu"
+              className="max-h-5 cursor-pointer"
+            />
+            <div className="absolute top-full right-0 z-30 w-32 p-5 round-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
+              <p
+                className="cursor-pointer text-sm"
+                onClick={() => navigate("/profile")}
+              >
+                Edit Profile
+              </p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
           </div>
         </div>
       </div>
@@ -27,7 +40,6 @@ const Sidebar = ({ selecteduser, setSelectedUser }) => {
           placeholder=" Search User..."
         />
       </div>
-
     </div>
   );
 };
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,28 +1,57 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import assets from '../assets/assets'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import assets from "../assets/assets";
 
 const ProfilePage = () => {
-    const [selectedImg,setSelectedImg] = useState(null)
-    const navigate = useNavigate()
-      const [name, setName] = useState("Martin Johnson");
-      const [bio, setBio] = useState("Hi everyone,I'm using quickchat");
-    
-  return (
-    <div className='min-h-screen border w-full bg-cover bg-no-repeat flex items-center justify-center'>
-        <div className='w-5/6 max-w-2xl backdroup-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
-        <form className='flex flex-col gap-5 p-10 flex-1'>
-            <h3 className='text-lg'>Profile Details</h3>
-            <label htmlFor='avatar' className='flex item-center gap-3 cursor-pointer'>
-                <input type='file' onChange={(e) => setSelectedImg(e.target.files[0])} id='avatar' accept='.png,.jpg,.jpeg' hidden />
-                <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt='' className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}/>
-                Upload profile Image
-            </label>
-        </form>
-        <img className='' alt=""></img>
-        </div>
-    </div>
-  )
-}
+  const [selectedImg, setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState("Martin Johnson");
+  const [bio, setBio] = useState("Hi everyone,I'm using quickchat");
 
-export default ProfilePage
+  return (
+    <div className="min-h-screen border w-full bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdroup-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile Details</h3>
+          <label
+            htmlFor="avatar"
+            className="flex item-center gap-3 cursor-pointer"
+          >
+            <input
+              type="file"
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              id="avatar"
+              accept=".png,.jpg,.jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImg
+                  ? URL.createObjectURL(selectedImg)
+                  : assets.avatar_icon
+              }
+              alt=""
+              className={`w-12 h-12 ${selectedImg && "rounded-full"}`}
+            />
+            Upload profile Image
+          </label>
+          <input placeholder="Your name" required="" class="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" type="text" value="jenish"></input>
+          <textarea placeholder="Write profile bio" required="" class="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" rows="4">kuch nahi he</textarea>
+          <button
+            type="submit"
+            class="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer"
+          >
+            Save
+          </button>
+        </form>
+        <img
+          src={assets.logo_icon}
+          className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10"
+          alt=""
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
